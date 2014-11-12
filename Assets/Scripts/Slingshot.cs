0ec32eb2ae4ded7820f692a9c339c6bed37a91c0
Denny Scott <dennyscott301@gmail.com>
2014-11-11 23:10:31 -0600

    Added Halo on Slingshot

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -2,13 +2,23 @@
 using System.Collections;
 
 public class Slingshot : MonoBehaviour {
+	public GameObject 		launchPoint;
+
+	void Awake() {
+		Transform  launchPointTrans = transform.Find ("LaunchPoint");
+		launchPoint = launchPointTrans.gameObject;
+		launchPoint.SetActive(false);
+	}
 
 	void OnMouseEnter() {
-		print ("Slingshot:OnMouseEnter()");
+//		print ("Slingshot:OnMouseEnter()");
+		launchPoint.SetActive (true);
+
 	}
 
 	void OnMouseExit() {
-		print ("Slingbot:OnMouseExit()");
+//		print ("Slingbot:OnMouseExit()");
+		launchPoint.SetActive(false);
 	}
 
 	// Use this for initialization
